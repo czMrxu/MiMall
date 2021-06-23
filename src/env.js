7ebcdf7f5e05c1c032994d4856ec917bc17146e0
czMrxu <1904378660@qq.com>
2021-06-24 00:01:56 +0800

    项目基础架构的完成

---
+++ src/env.js
@@ -0,0 +1,21 @@
+let baseURL;
+
+switch (process.env.NODE_ENV){
+    case 'development':
+        baseURL = 'http://dev-mall-pre.springboot.cn/api';
+        break;
+    case 'test':
+        baseURL = 'http://dev-mall-pre.springboot.cn/api';
+        break;
+    case 'production':
+        baseURL = 'http://dev-mall-pre.springboot.cn/api';
+        break;
+    default:
+        baseURL = 'http://dev-mall-pre.springboot.cn/api';
+        break;
+}
+
+
+export default{
+    baseURL
+}
